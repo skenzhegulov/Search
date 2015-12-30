cefe71e6981cfc8f249f122440000787bb799976
Saken Kenzhegulov <saken.aktl@gmail.com>
2015-12-30 17:36:27 +0900

    Task #2

---
+++ Search/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Exam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.f_path = new System.Windows.Forms.TextBox();
+            this.txt_keyword = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.lbl_folder = new System.Windows.Forms.Label();
+            this.lbl_keyword = new System.Windows.Forms.Label();
+            this.data_results = new System.Windows.Forms.DataGridView();
+            this.btn_exp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.data_results)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(486, 33);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(80, 20);
+            this.btn_browse.TabIndex = 0;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_result_Click);
+            // 
+            // f_path
+            // 
+            this.f_path.Location = new System.Drawing.Point(12, 33);
+            this.f_path.Name = "f_path";
+            this.f_path.Size = new System.Drawing.Size(443, 20);
+            this.f_path.TabIndex = 1;
+            // 
+            // txt_keyword
+            // 
+            this.txt_keyword.Location = new System.Drawing.Point(12, 85);
+            this.txt_keyword.Name = "txt_keyword";
+            this.txt_keyword.Size = new System.Drawing.Size(443, 20);
+            this.txt_keyword.TabIndex = 2;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(486, 85);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(80, 20);
+            this.btn_search.TabIndex = 3;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // lbl_folder
+            // 
+            this.lbl_folder.AutoSize = true;
+            this.lbl_folder.Location = new System.Drawing.Point(12, 17);
+            this.lbl_folder.Name = "lbl_folder";
+            this.lbl_folder.Size = new System.Drawing.Size(29, 13);
+            this.lbl_folder.TabIndex = 4;
+            this.lbl_folder.Text = "Path";
+            // 
+            // lbl_keyword
+            // 
+            this.lbl_keyword.AutoSize = true;
+            this.lbl_keyword.Location = new System.Drawing.Point(12, 69);
+            this.lbl_keyword.Name = "lbl_keyword";
+            this.lbl_keyword.Size = new System.Drawing.Size(48, 13);
+            this.lbl_keyword.TabIndex = 5;
+            this.lbl_keyword.Text = "Keyword";
+            // 
+            // data_results
+            // 
+            this.data_results.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data_results.Location = new System.Drawing.Point(12, 130);
+            this.data_results.Name = "data_results";
+            this.data_results.ReadOnly = true;
+            this.data_results.Size = new System.Drawing.Size(554, 216);
+            this.data_results.TabIndex = 6;
+            this.data_results.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.data_results_CellClick);
+            // 
+            // btn_exp
+            // 
+            this.btn_exp.Location = new System.Drawing.Point(486, 358);
+            this.btn_exp.Name = "btn_exp";
+            this.btn_exp.Size = new System.Drawing.Size(75, 23);
+            this.btn_exp.TabIndex = 7;
+            this.btn_exp.Text = "Export";
+            this.btn_exp.UseVisualStyleBackColor = true;
+            this.btn_exp.Click += new System.EventHandler(this.btn_exp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 393);
+            this.Controls.Add(this.btn_exp);
+            this.Controls.Add(this.data_results);
+            this.Controls.Add(this.lbl_keyword);
+            this.Controls.Add(this.lbl_folder);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.txt_keyword);
+            this.Controls.Add(this.f_path);
+            this.Controls.Add(this.btn_browse);
+            this.Name = "Form1";
+            this.Text = "Search";
+            ((System.ComponentModel.ISupportInitialize)(this.data_results)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.TextBox f_path;
+        private System.Windows.Forms.TextBox txt_keyword;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Label lbl_folder;
+        private System.Windows.Forms.Label lbl_keyword;
+        private System.Windows.Forms.DataGridView data_results;
+        private System.Windows.Forms.Button btn_exp;
+    }
+}
+
---
+++ Search/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using GemBox.Spreadsheet;
+
+namespace Exam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+        }
+        DataGridView data;
+
+        private void InitializeDataGridView()
+        {
+            data = this.data_results;
+            data.ColumnCount = 5;
+            data.Columns[0].Name = "Filename";
+            data.Columns[1].Name = "Path";
+            data.Columns[2].Name = "Size";
+            data.Columns[3].Name = "Cell"; //worksheet and cell name
+            data.Columns[4].Name = "Text"; //found text
+            data.AllowUserToAddRows = false;
+            data.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void btn_result_Click(object sender, EventArgs e)
+        {
+            string folderPath = "";
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+            }
+            this.f_path.Text = folderPath;
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            string folderPath = this.f_path.Text;
+            string keyword = this.txt_keyword.Text;
+
+            do
+            {
+                foreach (DataGridViewRow row in data.Rows)
+                {
+                    try
+                    {
+                        data.Rows.Remove(row);
+                    }
+                    catch (Exception) { }
+                }
+            } while (data.Rows.Count > 0);
+
+            try
+            {
+                string[] dirs = Directory.GetFiles(folderPath, "*", SearchOption.AllDirectories);
+
+                foreach (string dir in dirs)
+                {
+                    string filename = Path.GetFileName(dir);
+                    string length = getSize(dir);
+                    string ext = Path.GetExtension(dir);
+                    string cell = "";
+                    string text = null;
+
+                    if (ext.Equals(".txt")) text = getLine(dir, keyword); else 
+                    if (ext.Equals(".xls") || ext.Equals(".xlsx")) getCell(dir, keyword, ext, filename, length); else
+                    if (System.Text.RegularExpressions.Regex.IsMatch(filename, keyword)) text = "";
+                    else continue;
+
+                    if (text != null)
+                    {
+                        string[] row = new string[] { filename, dir, length, cell, text };
+                        data.Rows.Add(row);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The process failed: {0}", ex.ToString());
+            }
+        }
+
+        string[] sizes = { "B", "KB", "MB", "GB" };
+
+        private string getSize(string dir)
+        {
+            long len = new System.IO.FileInfo(dir).Length;
+            int order = 0;
+            while (len >= 1024 && order + 1 < sizes.Length)
+            {
+                order++;
+                len = len / 1024;
+            }
+            return String.Format("{0:0.##} {1}", len, sizes[order]);
+        }
+
+        private string getLine(string dir, string key)
+        {
+            using (var reader = new StreamReader(dir))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (System.Text.RegularExpressions.Regex.IsMatch(line, key)) return line;
+                }
+                return null;
+            }
+        }
+
+        [STAThread]
+        private void getCell(string dir, string key, string ext, 
+                             string filename, string length)
+        {
+            SpreadsheetInfo.SetLicense("EIKU-1Y5V-Y7R4-ASNB");
+
+            ExcelFile ef = new ExcelFile();
+            if (ext.Equals(".xlsx")) ef.LoadXlsx(dir, XlsxOptions.None);
+            else ef.LoadXls(dir);
+
+            
+            string cell;
+            string text;
+
+            foreach (ExcelWorksheet sheet in ef.Worksheets)
+            {
+                foreach (var currnetRow in sheet.Rows)
+                {
+                    int row;
+                    int col;
+                    if (currnetRow.Cells.FindText(key, false, false, out row, out col))
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append(sheet.Cells[row, col].Value);
+                        text = sb.ToString();
+                        cell = sheet.Name+"/"+CellRange.RowColumnToPosition(row, col);
+                        string[] nrow = new string[] { filename, dir, length, cell, text };
+                        data.Rows.Add(nrow);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void data_results_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string dir = data.Rows[e.RowIndex].Cells[1].Value.ToString();
+            Console.WriteLine("{0}", dir);
+            System.Diagnostics.Process.Start(dir);
+        }
+
+        [STAThread]
+        private void btn_exp_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            foreach (DataGridViewColumn col in data.Columns)
+            {
+                dt.Columns.Add(col.HeaderText);
+            }
+
+            foreach (DataGridViewRow row in data.Rows)
+            {
+                DataRow dRow = dt.NewRow();
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    dRow[cell.ColumnIndex] = cell.Value;
+                }
+                dt.Rows.Add(dRow);
+            }
+            
+            Stream myStream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "XLS files (*.xls)|*.xls|XLT files (*.xlt)|*.xlt|XLSX files (*.xlsx)|*.xlsx|XLSM files (*.xlsm)|*.xlsm|XLTX (*.xltx)|*.xltx|XLTM (*.xltm)|*.xltm|ODS (*.ods)|*.ods|OTS (*.ots)|*.ots|CSV (*.csv)|*.csv|TSV (*.tsv)|*.tsv|HTML (*.html)|*.html|MHTML (.mhtml)|*.mhtml|PDF (*.pdf)|*.pdf|XPS (*.xps)|*.xps|BMP (*.bmp)|*.bmp|GIF (*.gif)|*.gif|JPEG (*.jpg)|*.jpg|PNG (*.png)|*.png|TIFF (*.tif)|*.tif|WMP (*.wdp)|*.wdp";
+            saveFileDialog.FilterIndex = 3;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                    ExcelFile ef = new ExcelFile();
+                    ExcelWorksheet ws = ef.Worksheets.Add("Sheet1");
+                    ws.InsertDataTable(dt, 0, 0, true);
+                    ef.SaveXlsx(saveFileDialog.FileName);
+            }
+            
+        }
+    }
+}
